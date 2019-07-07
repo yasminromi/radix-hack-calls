241f80e18806404930aa2723f7b9232ffdf2a939
yasminromi <yasmin.romi@al.infnet.edu.br>
2019-07-06 21:26:39 -0300

    making call

--- make_call.py
+++ make_call.py
@@ -1,16 +1,12 @@
 from twilio.rest import Client
 
-account_sid = 'ACcd5071d8b3949b7dd9b0a65a3b6dd58f'
-auth_token = 'c03e65954bb6c81e42aedc7601e5942e'
+account_sid = 'ACf737a865f5030947224ce86063f4806b'
+auth_token = 'bebc51d8e5fc8678825df24190968d82'
 client = Client(account_sid, auth_token)
 
-def lambda_handler(event, context):
     
-    call = client.calls.create( url='http://demo.twilio.com/docs/voice.xml',
-                                to='+5521965636086',
-                                from_='+551149496211')
-                        
-    return {
-            'statusCode': 200,
-            'body': json.dumps(call.sid)
-    }
+call = client.calls.create( url='https://raw.githubusercontent.com/yasminromi/radix-hack-calls/master/call.xml',
+                            to='+5521986858723',
+                            from_='+551149507127')
+                    
+print(call.sid)
